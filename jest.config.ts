63c5dbf83ab6e4b0faddf86143c5f64d37a2d416
Kaique D Felipe <83593559+kaique-kira@users.noreply.github.com>
2025-05-14 17:41:58 -0300

    serasse

--- jest.config.ts
+++ jest.config.ts
@@ -4,11 +4,22 @@ module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/test/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['lcov', 'text'],
   reporters: [
     'default',
     [
       'jest-junit',
-      { outputDirectory: 'test-results', outputName: 'test-report.xml' },
+      {
+        outputDirectory: 'test-results',
+        outputName: 'test-report.xml',
+        ancestorSeparator: ' › ',
+        uniqueOutputName: 'false',
+        suiteNameTemplate: '{filename}',
+        classNameTemplate: '{classname}',
+        titleTemplate: '{title}',
+      },
     ],
   ],
 };
